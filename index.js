1fd1eab894a46e681e9e6921dffd9b207241601a
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-07-21 19:48:31 +0530

    complete and connect everything in index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+//app create
+const express=require("express");
+const app=express();
+
+//PORT
+const PORT=process.env.PORT || 5000
+
+//Middleware
+app.use(express.json());
+const fileUpload=require("express-fileupload");
+app.use(fileUpload());
+
+//db connection
+const db=require("./config/database");
+db.connect();
+
+//cloud connection
+const cloudinary=require("./config/cloudinary");
+cloudinary.cloudinaryConnect();
+
+//api rout mounting
+const Upload=require("./routes/FileUpload");
+app.use('api/v1' , Upload);
+
+//activate server
+app.listen(PORT , ()=>{
+    console.log(`App is running at ${PORT}`);
+})
