0acd0bd698fd7668f34742749505fc4f030ab06e
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-07-22 13:45:13 +0530

    add sending mail functionality

--- models/File.js
+++ models/File.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose");
-
+const nodemailer=require("nodemailer")
+require("dotenv").config();
 const fileSchema=new mongoose.Schema({
     name:{
         type:String,
@@ -15,6 +16,33 @@ const fileSchema=new mongoose.Schema({
         type:String
     }
 });
+
+
+//post middleware
+
+fileSchema.post("save" , async function(doc){
+    try {
+        console.log("doc :" , doc);
+        //transporter
+        let transporter=nodemailer.transporter({
+            host:process.env.MAIL_HOSt,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass:process.env.MAIL_PASS
+            }
+        });
+        //send mail
+        let info=await transporter.sendMail({
+            from:`fileUploader`,
+            to:doc.email,
+            subject:"new file uploaded",
+            html:"new file uploader go check"
+        })
+
+    } catch (error) {
+        console.error(error) 
+    }
+})
 
 const File=mongoose.model("File" , fileSchema);
 module.exports=File;
